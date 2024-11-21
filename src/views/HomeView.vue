4d3dc242e5acf2ab566cb8116d68ff4971e19bec
Debapratim Bhattacharjee <debagourbhatta@gmail.com>
2024-11-21 23:54:56 +0530

    Initial commit for Uber Clone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="Home" class="flex flex-col h-screen">
+    <div class="m-3 mb-auto">
+      <!-- Grid layout for services -->
+      <div class="grid grid-cols-2 gap-4 my-4">
+        <ServiceSelectLarge
+          text="Ride"
+          imagewidth="100"
+          image="ride"
+        />
+        <ServiceSelectLarge
+          text="Food"
+          imagewidth="100"
+          image="food"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue';
 </script>
+
+<style scoped>
+/* Grid-specific styles */
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); /* Two equal columns */
+  gap: 19px; /* Space between items */
+}
+
+#Home {
+  padding: 10px;
+}
+</style>
